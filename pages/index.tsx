2e6a7274bc98ad39fccadb484f75761d15b9ffcc
bggbr <48645202+bggbr@users.noreply.github.com>
2022-12-04 14:11:30 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,21 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import homeStyles from '../styles/Home.module.css';
+
 export default function Home() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return (
+        <div className={homeStyles.container}>
+            <Head>
+                <title className="">BOB</title>
+            </Head>
+            <section className={homeStyles.headingMd}>
+                <p>[BOB Introduction]</p>
+                <p>(This is a website)</p>
+            </section>
+            <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
+                <h2 className={homeStyles.headingLg}>Blog</h2>
+                <ul className={homeStyles.list}></ul>
+            </section>
+        </div>
+    );
 }
