4e99df11cdb309a2d927eb12980403eacf3fe3f6
bggbr <jaejung603@gmail.com>
2022-12-10 11:58:34 +0900

    md 파일 작성 및 home 렌더링

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9653ceabeb9adf9e"; }
+/******/ 		__webpack_require__.h = function() { return "5f16bd776590372f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import homeStyles from '../styles/Home.module.css';
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { getSortedPostsData } from "../lib/post";
+import homeStyles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({
+    allPostsData,
+}: {
+    allPostsData: {
+        date: string;
+        title: string;
+        id: string;
+    }[];
+}) {
     return (
         <div className={homeStyles.container}>
             <Head>
-                <title className="">BOB</title>
+                <title className=''>BOB</title>
             </Head>
             <section className={homeStyles.headingMd}>
                 <p>[BOB Introduction]</p>
@@ -14,8 +24,27 @@ export default function Home() {
             </section>
             <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
                 <h2 className={homeStyles.headingLg}>Blog</h2>
-                <ul className={homeStyles.list}></ul>
+                <ul className={homeStyles.list}>
+                    {allPostsData.map(({ id, title, date }) => (
+                        <li className={homeStyles.listItem} key={id}>
+                            <a>{title}</a>
+                            <br />
+                            <small className={homeStyles.lightText}>{date}</small>
+                        </li>
+                    ))}
+                </ul>
             </section>
         </div>
     );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+    const allPostsData = getSortedPostsData();
+    console.log(allPostsData);
+
+    return {
+        props: {
+            allPostsData,
+        },
+    };
+};
